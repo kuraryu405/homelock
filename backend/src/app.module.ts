e0ad36405b84b13eea1ebaf8d4ba7a1cd4dca181
くらりゅ！ <s1f102501260@iniad.org>
2026-02-02 14:56:51 +0900

    backend/rewrite appmodule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,38 @@ import { TokenModule } from './token/token.module';
 import { BotModule } from './bot/bot.module';
 import { UnlockModule } from './unlock/unlock.module';
 import { LogModule } from './log/log.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Token } from './token/entities/token.entity';
+import { Log } from './log/entities/log.entity';
 
 @Module({
-  imports: [TokenModule, BotModule, UnlockModule, LogModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST', 'localhost'),
+        port: config.get<number>('DB_PORT', 5432),
+        username: config.get<string>('DB_USERNAME', 'user'),
+        password: config.get<string>('DB_PASSWORD', 'password'),
+        database: config.get<string>('DB_DATABASE', 'homelock_db'),
+        entities: [Token, Log],
+        synchronize: true, // 開発中は自動でテーブル作成
+        logging: true, // SQLの中身をターミナルに表示
+      }),
+    }),
+
+    TokenModule,
+    BotModule,
+    UnlockModule,
+    LogModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
