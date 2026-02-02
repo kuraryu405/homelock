63ee0695c1e4d0066c8eb137b44638ef116675ab
くらりゅ！ <s1f102501260@iniad.org>
2026-02-02 12:23:49 +0900

    backend/db create token-entity

--- backend/src/token/entities/token.entity.ts
+++ backend/src/token/entities/token.entity.ts
@@ -1,1 +1,23 @@
-export class Token {}
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, Index } from 'typeorm';
+
+@Entity('Tokens')
+export class Token {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  @Index()
+  token: string;
+
+  @Column({ type: 'timestamp' })
+  expiration_date: Date;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+  
+  @Column({ nullable: true })
+  memo?: string;
+
+  @Column({ default: true})
+  is_active: boolean;
+}
